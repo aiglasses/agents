5314f5efe3b82abfa4ff70b9e7d19eb0563df191
Gavin <43789460+Gavingx@users.noreply.github.com>
2025-02-17 12:22:23 +0800

    Update client_initializer.py

--- custom_agent_demos/examples/custom_agent/client_initializer.py
+++ custom_agent_demos/examples/custom_agent/client_initializer.py
@@ -2,7 +2,7 @@
 import os
 from food_agent import FoodAgent
 
-MODEL = "qwen-plus"
+MODEL = "qwen-vl-plus"
 API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL")
-agent_client = FoodAgent(model="qwen-plus", api_key=API_KEY, base_url=BASE_URL)
+agent_client = FoodAgent(model=MODEL, api_key=API_KEY, base_url=BASE_URL)
